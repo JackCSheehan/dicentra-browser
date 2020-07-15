ba256453d759d384b9e066de792c9df5cced29ef
Jack <jaxanimations@gmail.com>
2020-07-15 00:00:22 -0500

    Implemented tabs

--- Dicentra/Dicentra/Form1.Designer.cs
+++ Dicentra/Dicentra/Form1.Designer.cs
@@ -29,49 +29,82 @@
         private void InitializeComponent()
         {
             this.controlsPanel = new System.Windows.Forms.Panel();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.urlPanel = new System.Windows.Forms.Panel();
+            this.urlBar = new System.Windows.Forms.TextBox();
             this.newTabButton = new System.Windows.Forms.Button();
             this.homeButton = new System.Windows.Forms.Button();
             this.refreshButton = new System.Windows.Forms.Button();
             this.forwardButton = new System.Windows.Forms.Button();
-            this.urlBar = new System.Windows.Forms.TextBox();
             this.backButton = new System.Windows.Forms.Button();
-            this.browser = new Microsoft.Toolkit.Forms.UI.Controls.WebView();
-            this.tab = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.tabsPanel = new System.Windows.Forms.Panel();
+            this.defaultWebView = new Microsoft.Toolkit.Forms.UI.Controls.WebView();
             this.controlsPanel.SuspendLayout();
+            this.tabControl.SuspendLayout();
             this.urlPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.browser)).BeginInit();
-            this.tabsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.defaultWebView)).BeginInit();
             this.SuspendLayout();
             // 
             // controlsPanel
             // 
             this.controlsPanel.BackColor = System.Drawing.Color.Transparent;
+            this.controlsPanel.Controls.Add(this.tabControl);
             this.controlsPanel.Controls.Add(this.urlPanel);
-            this.controlsPanel.Controls.Add(this.tabsPanel);
             this.controlsPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.controlsPanel.Location = new System.Drawing.Point(0, 0);
             this.controlsPanel.Name = "controlsPanel";
             this.controlsPanel.Size = new System.Drawing.Size(800, 50);
             this.controlsPanel.TabIndex = 0;
             // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(800, 20);
+            this.tabControl.TabIndex = 2;
+            this.tabControl.SelectedIndexChanged += new System.EventHandler(this.tabControl_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 0);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "New Tab";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
             // urlPanel
             // 
             this.urlPanel.AutoSize = true;
             this.urlPanel.BackColor = System.Drawing.Color.Transparent;
+            this.urlPanel.Controls.Add(this.urlBar);
             this.urlPanel.Controls.Add(this.newTabButton);
             this.urlPanel.Controls.Add(this.homeButton);
             this.urlPanel.Controls.Add(this.refreshButton);
             this.urlPanel.Controls.Add(this.forwardButton);
-            this.urlPanel.Controls.Add(this.urlBar);
             this.urlPanel.Controls.Add(this.backButton);
             this.urlPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.urlPanel.Location = new System.Drawing.Point(0, 21);
             this.urlPanel.Name = "urlPanel";
             this.urlPanel.Size = new System.Drawing.Size(800, 29);
             this.urlPanel.TabIndex = 1;
+            // 
+            // urlBar
+            // 
+            this.urlBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.urlBar.Location = new System.Drawing.Point(208, 3);
+            this.urlBar.Name = "urlBar";
+            this.urlBar.Size = new System.Drawing.Size(588, 23);
+            this.urlBar.TabIndex = 6;
+            this.urlBar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.urlBar_KeyDown);
             // 
             // newTabButton
             // 
@@ -84,6 +117,7 @@
             this.newTabButton.TabIndex = 5;
             this.newTabButton.Text = "＋";
             this.newTabButton.UseVisualStyleBackColor = true;
+            this.newTabButton.Click += new System.EventHandler(this.newTabButton_Click);
             // 
             // homeButton
             // 
@@ -124,17 +158,6 @@
             this.forwardButton.UseVisualStyleBackColor = true;
             this.forwardButton.Click += new System.EventHandler(this.forwardButton_Click);
             // 
-            // urlBar
-            // 
-            this.urlBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.urlBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.urlBar.Location = new System.Drawing.Point(205, 3);
-            this.urlBar.Name = "urlBar";
-            this.urlBar.Size = new System.Drawing.Size(592, 23);
-            this.urlBar.TabIndex = 1;
-            this.urlBar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.urlBar_KeyDown);
-            // 
             // backButton
             // 
             this.backButton.FlatAppearance.BorderSize = 0;
@@ -148,69 +171,35 @@
             this.backButton.UseVisualStyleBackColor = true;
             this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
-            // browser
+            // defaultWebView
             // 
-            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.browser.Location = new System.Drawing.Point(0, 50);
-            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
-            this.browser.Name = "browser";
-            this.browser.Size = new System.Drawing.Size(800, 400);
-            this.browser.Source = new System.Uri("https://google.com", System.UriKind.Absolute);
-            this.browser.TabIndex = 1;
-            this.browser.DOMContentLoaded += new System.EventHandler<Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlDOMContentLoadedEventArgs>(this.browser_DOMContentLoaded);
-            this.browser.NavigationStarting += new System.EventHandler<Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationStartingEventArgs>(this.browser_NavigationStarting);
-            // 
-            // tab
-            // 
-            this.tab.FlatAppearance.BorderColor = System.Drawing.Color.Gainsboro;
-            this.tab.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.tab.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.tab.Location = new System.Drawing.Point(0, 0);
-            this.tab.Name = "tab";
-            this.tab.Size = new System.Drawing.Size(130, 23);
-            this.tab.TabIndex = 0;
-            this.tab.Text = "New Tab";
-            this.tab.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Gainsboro;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.button1.Location = new System.Drawing.Point(136, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(130, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "New Tab";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // tabsPanel
-            // 
-            this.tabsPanel.BackColor = System.Drawing.Color.Transparent;
-            this.tabsPanel.Controls.Add(this.button1);
-            this.tabsPanel.Controls.Add(this.tab);
-            this.tabsPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.tabsPanel.Location = new System.Drawing.Point(0, 0);
-            this.tabsPanel.Name = "tabsPanel";
-            this.tabsPanel.Size = new System.Drawing.Size(800, 24);
-            this.tabsPanel.TabIndex = 0;
-            this.tabsPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.tabsPanel_Paint);
+            this.defaultWebView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.defaultWebView.Location = new System.Drawing.Point(0, 50);
+            this.defaultWebView.MinimumSize = new System.Drawing.Size(20, 20);
+            this.defaultWebView.Name = "defaultWebView";
+            this.defaultWebView.Size = new System.Drawing.Size(800, 400);
+            this.defaultWebView.Source = new System.Uri("https://google.com", System.UriKind.Absolute);
+            this.defaultWebView.TabIndex = 1;
+            this.defaultWebView.DOMContentLoaded += new System.EventHandler<Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlDOMContentLoadedEventArgs>(this.browser_DOMContentLoaded);
+            this.defaultWebView.NavigationCompleted += new System.EventHandler<Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationCompletedEventArgs>(this.browser_NavigationCompleted);
+            this.defaultWebView.NavigationStarting += new System.EventHandler<Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationStartingEventArgs>(this.browser_NavigationStarting);
             // 
             // BrowserForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.browser);
+            this.Controls.Add(this.defaultWebView);
             this.Controls.Add(this.controlsPanel);
             this.Name = "BrowserForm";
             this.Text = "Dicentra";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.BrowserForm_FormClosed);
             this.controlsPanel.ResumeLayout(false);
             this.controlsPanel.PerformLayout();
+            this.tabControl.ResumeLayout(false);
             this.urlPanel.ResumeLayout(false);
             this.urlPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.browser)).EndInit();
-            this.tabsPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.defaultWebView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -221,14 +210,14 @@
         private System.Windows.Forms.Panel urlPanel;
         private System.Windows.Forms.Button backButton;
         private System.Windows.Forms.Button forwardButton;
-        private System.Windows.Forms.TextBox urlBar;
+        private System.Windows.Forms.TextBox rlBar;
         private System.Windows.Forms.Button refreshButton;
         private System.Windows.Forms.Button homeButton;
         private System.Windows.Forms.Button newTabButton;
-        private Microsoft.Toolkit.Forms.UI.Controls.WebView browser;
-        private System.Windows.Forms.Panel tabsPanel;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button tab;
+        private Microsoft.Toolkit.Forms.UI.Controls.WebView defaultWebView;
+        private System.Windows.Forms.TextBox urlBar;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
     }
 }
 
--- Dicentra/Dicentra/Form1.cs
+++ Dicentra/Dicentra/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Forms.UI.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,24 +8,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Windows.AI.MachineLearning;
 
 namespace Dicentra
 {
     public partial class BrowserForm : Form
     {
 
-        private String homeUrl = "https://google.com";  // URL of home page
+        private String homeUrl = "https://google.com";          // URL of home page
+        private int tabIndex = 0;                               // Index of currently selected tab
+        private List<WebView> webViews = new List<WebView>();   // List of web views for each tab
+        private WebView currentWebView = new WebView();         // Currently selected webview
+
 
         public BrowserForm()
         {
             InitializeComponent();
+
+            // Add default web view to web views list
+            webViews.Add(defaultWebView);
+
+            // Assign current web view to default web view
+            currentWebView = defaultWebView;
         }
 
         // Highlights home button or unhighlights it depending if the current page is the home page
         private void highlightHomeButton()
         {
             // Set color of home button to be highlighted when current page is home page
-            if (browser.Source.ToString() == homeUrl)
+            if (currentWebView.Source.ToString() == homeUrl)
             {
                 homeButton.BackColor = System.Drawing.Color.Orange;
             }
@@ -34,19 +46,37 @@ namespace Dicentra
             }
         }
 
+        // Updates text of current tab, form title, and URL bar to match the content of the current web view
+        private void updatePageTextInfo()
+        {
+            // Set form title to title of webpage
+            this.Text = currentWebView.DocumentTitle;
+
+            // Set text of url bar to be current page's title
+            urlBar.Text = currentWebView.Source.ToString();
+
+            // Unfocus url bar once content is loaded
+            this.ActiveControl = null;
+
+            // Change text of current tab to be the name of the current webview
+            tabControl.SelectedTab.Text = currentWebView.DocumentTitle;
+
+            highlightHomeButton();
+        }
+
         private void backButton_Click(object sender, EventArgs e)
         {
-            browser.GoBack();
+            currentWebView.GoBack();
         }
 
         private void forwardButton_Click(object sender, EventArgs e)
         {
-            browser.GoForward();
+            currentWebView.GoForward();
         }
 
         private void refreshButton_Click(object sender, EventArgs e)
         {
-            browser.Refresh();
+            currentWebView.Refresh();
         }
 
         private void homeButton_MouseDown(object sender, MouseEventArgs e)
@@ -54,12 +84,12 @@ namespace Dicentra
             // If user clicks with left mouse button, go to home page
             if (e.Button == MouseButtons.Left)
             {
-                browser.Navigate(homeUrl);
+                currentWebView.Navigate(homeUrl);
             }
             // If user clicks with right mouse button, set home page
             else if (e.Button == MouseButtons.Right)
             {
-                homeUrl = browser.Source.ToString();
+                homeUrl = currentWebView.Source.ToString();
                 highlightHomeButton();
             }
         }
@@ -84,36 +114,93 @@ namespace Dicentra
                     urlBar.Text = urlBarText;
 
                     // Set browser URL to the validated text put into the URL bar
-                    browser.Navigate(urlBarText);
+                    currentWebView.Navigate(urlBarText);
 
                 }
                 catch (Exception) { }
             }
         }
 
+        // Handler for when webpage finishes loading
         private void browser_DOMContentLoaded(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlDOMContentLoadedEventArgs e)
         {
-            // Set form title to title of webpage
-            this.Text = browser.DocumentTitle;
-
-            // Set text of url bar to be current page's title
-            urlBar.Text = browser.Source.ToString();
-
             // Unfocus url bar once content is loaded
-            this.ActiveControl = null;
+            this.ActiveControl = currentWebView;
 
-            highlightHomeButton();
+            updatePageTextInfo();
         }
 
         private void browser_NavigationStarting(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationStartingEventArgs e)
         {
-            // Set title to loading to indicate that the current page is loading
+            // Set title of form and current tab to loading to indicate that the current page is loading
             this.Text = "Loading...";
+            tabControl.SelectedTab.Text = "Loading...";
+
         }
 
-        private void tabsPanel_Paint(object sender, PaintEventArgs e)
-        {
+        private void tabsPanel_Paint(object sender, PaintEventArgs e) {}
 
+        // Handler for adding a new tab to browser.
+        private void newTabButton_Click(object sender, EventArgs e)
+        {
+            // Create new webview and set its settings
+            WebView newWebView = new WebView();
+            newWebView.Source = new Uri(homeUrl);
+            newWebView.Dock = DockStyle.Fill;
+
+            // Add event handlers to new webview
+            newWebView.NavigationCompleted += new EventHandler<Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationCompletedEventArgs>(browser_NavigationCompleted);
+            newWebView.NavigationStarting += new EventHandler<Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationStartingEventArgs>(browser_NavigationStarting);
+            newWebView.DOMContentLoaded += new EventHandler<Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlDOMContentLoadedEventArgs>(browser_DOMContentLoaded);
+
+            // Add the new web view to the web view list
+            webViews.Add(newWebView);
+
+            // Add and select a new tab to the tab control
+            TabPage newTabPage = new TabPage("New Tab");
+            tabControl.TabPages.Add(newTabPage);
+            tabControl.SelectedTab = newTabPage;
+
+            // Change tab index to correspond to selected index of tab control
+            tabIndex = tabControl.SelectedIndex;
+
+            // Add the new tab web view and bring it to the front
+            this.Controls.Add(newWebView);
+            newWebView.BringToFront();
+        }
+
+        // Handler for when user changes tabs
+        private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Update tab index
+            tabIndex = tabControl.SelectedIndex;
+
+            // Update current web view when user changes tab
+            currentWebView = webViews.ElementAt(tabIndex);
+
+            // Bring webview up front depending on which tab user selected
+            currentWebView.BringToFront();
+
+            // Update page text  info when tab has been changed
+            updatePageTextInfo();
+        }
+
+        // Once page nagivation has started, update page text info
+        private void browser_NavigationCompleted(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationCompletedEventArgs e)
+        {
+            updatePageTextInfo();
+        }
+
+        // Handler for when form is closed
+        private void BrowserForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Go through each webview and destroy it
+            foreach (WebView webView in webViews)
+            {
+                // Stop all navigation and downloads and then dispose the web view
+                webView.Stop();
+                webView.Dispose();
+            }
         }
     }
 }
